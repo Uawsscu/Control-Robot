0d952ba5b2c1554736fea50d0471f080af4cb067
uawsscu <nonlapun_28@hotmail.com>
2017-09-06 00:50:14 +0700

    This is my project

---
+++ SpeechToText.py
@@ -0,0 +1,51 @@
+from os import environ, path
+import pyaudio
+import sys
+from pocketsphinx.pocketsphinx import *
+from sphinxbase.sphinxbase import *
+
+MODELDIR = "/home/uawsscu/PycharmProjects/Project2/model"
+DATADIR = "/home/uawsscu/PycharmProjects/Project2/data"
+
+config = Decoder.default_config()
+config.set_string('-hmm', path.join(MODELDIR, 'en-us/en-us'))
+config.set_string('-lm', path.join(MODELDIR, 'en-us/en-us.lm.bin'))
+config.set_string('-dict', path.join(MODELDIR, 'en-us/cmudict-en-us.dict'))
+decoder = Decoder(config)
+
+# Switch to JSGF grammar
+jsgf = Jsgf(path.join(DATADIR, 'sentence.gram'))
+rule = jsgf.get_rule('sentence.move') #>> public <move>
+fsg = jsgf.build_fsg(rule, decoder.get_logmath(), 7.5)
+fsg.writefile('sentence.fsg')
+
+decoder.set_fsg("sentence", fsg)
+decoder.set_search("sentence")
+
+p = pyaudio.PyAudio()
+stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=1024)
+stream.start_stream()
+
+in_speech_bf = False
+decoder.start_utt()
+while True:
+    buf = stream.read(1024)
+    if buf:
+        decoder.process_raw(buf, False, False)
+        if decoder.get_in_speech():
+            sys.stdout.write('.')
+            sys.stdout.flush()
+        if decoder.get_in_speech() != in_speech_bf:
+            in_speech_bf = decoder.get_in_speech()
+            if not in_speech_bf:
+                decoder.end_utt()
+                try:
+                    if decoder.hyp().hypstr != '':
+                        print 'Stream decoding result:', decoder.hyp().hypstr
+                except AttributeError:
+                    pass
+                decoder.start_utt()
+    else:
+        break
+decoder.end_utt()
+print('An Error occured:', decoder.hyp().hypstr)
